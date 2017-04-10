e7d9705f799e8168d24bf4bcc11f7ec1c6227ea3
Rob <robflettcc@gmail.com>
2017-04-10 17:20:43 +0100

    basic function added

--- public/app.js
+++ public/app.js
@@ -1,11 +1,38 @@
-var addCat = function(name, food, image){
- var ul = createUl();
- var li = createLi(name, food, image);
+// var addCat = function(name, food, image){
+//  var ul = createUl();
+//  var li = createLi(name, food);
+//  var img = createImg(image);
 
- appendElements(name, food, image);
-}
+//  appendElements(name, food, image);
+// }
 
 var app = function(){
+
+  var ul = document.createElement("ul");
+  ul.classList.add("cat");
+
+  var li = document.createElement("li");
+  li.innerText = "Name: Fredo"
+
+  var li2 = document.createElement("li2");
+  li2.innerText = "Favourite Food: Fredos"
+
+
+
+  var img = document.createElement("img");
+  img.width = "500"
+  img.src = "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg"
+ 
+  li.appendChild(img);
+
+  ul.appendChild(li);
+
+  ul.appendChild(li2);
+
+  
+
+  var cats = document.querySelector("#cats");
+  cats.appendChild(ul);
 
   // var article = document.createElement("article");
   // article.classList.add("quote");
